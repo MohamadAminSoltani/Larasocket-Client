f91eca452914d3c65d40f44aa79bb590ee65d159
M.A.Soltani <maxmadmas@gmail.com>
2022-04-08 01:26:20 +0430

    some changes

--- Larasocket.Shared/LarasocketRealTime.cs
+++ Larasocket.Shared/LarasocketRealTime.cs
@@ -1,7 +1,5 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net.WebSockets;
 using System.Reactive.Linq;
@@ -13,66 +11,49 @@ using System.Threading.Tasks;
 namespace Larasocket.Shared
 {
     /// <summary>
-    /// LarasocketRealTime
+    /// LarasocketClient
     /// The top-level class for the Ably Realtime library.
     /// </summary>
-    public class LarasocketRealTime
+    public class LarasocketClient
     {
-
-        public string ConnectionId { get; set; }
-        private SynchronizationContext _synchronizationContext;
-        internal ILogger Logger { get; private set; }
+        //private SynchronizationContext _synchronizationContext;
+        //internal ILogger Logger { get; private set; }
+        //internal volatile bool Disposed;
+        //private bool _disposing;
+        public string ConnectionId { get; private set; }
         private ClientWebSocket client;
         private CancellationTokenSource cts;
         private readonly string _token;
-        internal volatile bool Disposed;
-        private bool _disposing;
-        /// <summary>
-        /// Enable or disable text message conversion from binary to string (via 'MessageEncoding' property).
-        /// Default: true
-        /// </summary>
-        public bool IsTextMessageConversionEnabled { get; set; } = true;
-
-        internal LarasocketRealTime(string token)
+        private readonly string _userUuid;
+        private readonly Subject<ResponseMessage> _messageReceivedSubject = new Subject<ResponseMessage>();
+        public IObservable<ResponseMessage> MessageReceived => _messageReceivedSubject.AsObservable();
+        public LarasocketClient(string token,string userUuid)
         {
-            //Channels = new RealtimeChannels(this, Connection, mobileDevice);
-
-            //State = new RealtimeState(options.GetFallbackHosts()?.Shuffle().ToList(), options.NowFunc);
             _token = token;
-
+            _userUuid = userUuid;
             Connect();
         }
-       
 
         public async void Connect()
         {
-            if (Disposed)
-            {
-                throw new ObjectDisposedException("This instance has been disposed. Please create a new one.");
-            }
-
-
             client = new ClientWebSocket();
-            string guid = Guid.NewGuid().ToString();
-            var url = $"wss://ws.larasocket.com?token={_token}&uuid={guid}";
-
             cts = new CancellationTokenSource();
+            //string guid = Guid.NewGuid().ToString();
+            var url = $"wss://ws.larasocket.com?token={_token}&uuid={_userUuid}";
+
             await client.ConnectAsync(new Uri(url), cts.Token);
+
+            await Listen(client, cts);
 
             var connectMessage = new LinkMessage
             {
                 action = "link",
                 token = _token,
-                uuid = guid
+                uuid = _userUuid
             };
-
-            await Listen(client, cts);
-
-            SendMessageAsync(connectMessage);
+            await SendMessageAsync(connectMessage);
         }
-        private readonly Subject<ResponseMessage> _messageReceivedSubject = new Subject<ResponseMessage>();
-
-        public IObservable<ResponseMessage> MessageReceived => _messageReceivedSubject.AsObservable();
+        
 
         public void SubscribeToPublicChannel(string channelName)
         {
@@ -87,16 +68,14 @@ namespace Larasocket.Shared
             SendMessageAsync(messages);
         }
         
-        public async void SendMessageAsync<T>(T message)
+        public async Task SendMessageAsync<T>(T message)
         {
-
             string serialisedMessage = JsonConvert.SerializeObject(message);
 
             var byteMessage = Encoding.UTF8.GetBytes(serialisedMessage);
             var segmnet = new ArraySegment<byte>(byteMessage);
 
             await client.SendAsync(segmnet, WebSocketMessageType.Text, true, cts.Token);
-
         }
 
         private async Task Listen(WebSocket client, CancellationTokenSource token)
@@ -128,38 +107,6 @@ namespace Larasocket.Shared
                     } while (!result.EndOfMessage);
                 }
             }, cts.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
-
-        }
-
-        /// <summary>
-        /// Disposes the current instance.
-        /// Once disposed, it closes the connection and the library can't be used again.
-        /// </summary>
-        /// <param name="disposing">Whether the dispose method triggered it directly.</param>
-        public virtual void Dispose(bool disposing)
-        {
-            if (Disposed)
-            {
-                return;
-            }
-
-            if (disposing)
-            {
-                try
-                {
-                    //Connection?.RemoveAllListeners();
-                    //Channels?.CleanupChannels();
-                    //Push.Dispose();
-                }
-                catch (Exception e)
-                {
-                    Logger.Error("Error disposing Ably Realtime", e);
-                }
-            }
-
-            //Workflow.QueueCommand(DisposeCommand.Create().TriggeredBy($"AblyRealtime.Dispose({disposing}"));
-
-            Disposed = true;
         }
     }
 }
